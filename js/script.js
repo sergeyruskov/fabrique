3ecabade5a67dd47b109ba2b74ba3041ee539805
sergeyruskov <sergeyruskov@gmail.com>
2014-10-14 09:21:29 +0400

    5 day

--- js/script.js
+++ js/script.js
@@ -1,22 +1,32 @@
 $(function() {
 
-	var basketWrapper     = $(".basket-wrapper", '.header__first-row'),
-		basket            = $(".basket", ".basket-wrapper"),
-		basketNumberItems = $(".basket__number-items", ".basket-items"),
-		basketWordItems   = $("#basket__word-items", ".basket-items");
+	var basketWrapper      = $(".basket-wrapper", '.header__first-row'),
+		basketHeight       = $(".basket", ".basket-wrapper").outerHeight(),
+		basketButton       = $('.basket__button', '.basket'),
+		basketButtonIcon   = $('.basket__button__icon', '.basket__button'),
+		basketNumberItems  = $(".basket__number-items", ".basket-items"),
+		basketWordItems    = $("#basket__word-items", ".basket-items");
 
 	//Set basket height
 
-	if(basket.height() <= 45) {
-		basketWrapper.height('5em');
-	} else {
-		basketWrapper.height(basket.height());
+	if(basketHeight > 72) {
+		basketWrapper.height(basketHeight);
 	}
 
-	// Change item/items word in basket section
-	if(basketNumberItems.html() === ('1' || '0')) {
+	// Checking items in basket section
+	if(basketNumberItems.html() === '1' || basketNumberItems.html() === '0') {
 		basketWordItems.text('item');
+		basketNumberItems.css('color','#999');
+
+		if ( basketNumberItems.html() === '1' ) {
+			basketButtonIcon.css('color','#ad9');
+			basketButton.css('border-color','#ad9');
+		}
+
 	} else {
 		basketWordItems.text('items');
+		basketButtonIcon.css('color','#ad9');
+		basketButton.css('border-color','#ad9');
 	}
+
 });
